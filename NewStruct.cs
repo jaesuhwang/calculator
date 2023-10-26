92c89109197958653d85154a44246a54426d6506
jaesuhwang <westmilk85@gmail.com>
2023-10-26 19:21:26 +0900

    fewfwef

---
+++ NewStruct.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public struct NewStruct
+    {
+        
+   fjwieafjewjfl;wejaf;iewj;fjwieafjewjfl 
